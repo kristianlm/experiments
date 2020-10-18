20ed752340397ac6367abfef23be4b6e205441be
Kristian Lein-Mathisen <kristianlein@gmail.com>
2020-10-18 19:50:13 +0200

    build: rewrite example
    non-arduino style

--- kdb.c
+++ kdb.c
@@ -1,26 +1,28 @@
 #include "SSD1306_minimal.h"
 #include <avr/pgmspace.h>
+#include <util/delay.h>
 
 #define DEG "\xa7" "C"
 
-SSD1306_Mini oled; // Declare the OLED object
+SSD1306_Mini oled;
+
+const char hello[] = "Hello World!";
+
 
 void splash() {
  oled.startScreen();
- oled.clear(); // Clears the display
+ oled.clear();
 
- oled.cursorTo(0, 0); // x:0, y:0
- oled.printString("Hello World!");
- oled.cursorTo(0, 10); // x:0, y:23
+ oled.cursorTo(0, 0);
+ oled.printString(hello);
+ oled.cursorTo(0, 10);
  oled.printString("ATtiny85!");
 }
 
-void setup() {
- oled.init(0x3C); // Initializes the display to the specified address
- oled.clear(); // Clears the display
- delay(1000); // Delay for 1 second
- splash(); // Write something to the display (refer to the splash() method
-}
+int main() {
+  oled.init(0x78);
+  oled.clear();
 
-void loop() {
+  splash();
+  while(1);
 }
